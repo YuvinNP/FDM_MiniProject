ba51cc7153bcf9b5b091b6627adaa3aededf655e
YuvinNP <yuvinnimnaka1998@gmail.com>
2020-10-12 23:18:46 +0530

    latest

--- adr_operations.py
+++ adr_operations.py
@@ -3,51 +3,31 @@ import pandas as pd
 # data_frame = pd.read_csv( 'cluster_hotel.csv' )
 yearlist = []
 
-def toCSV(csv_file):
-    data_frame = pd.read_csv(csv_file)
-    return data_frame
+# def toCSV(csv_file):
+#     data_frame = pd.read_csv(csv_file)
+#     return data_frame
+
+def readCsv(file):
+    return pd.read_csv(file)
 
 import pandas as pd
 data_frame = pd.read_csv('correct_cluster.csv')
 yearlist = [int(2015), int(2016), int(2017)]
-def monthly_transaction_avg(data_frame):
-    # data_frame = pd.read_csv(source_file)
-    print( 'Loading...................' )
 
+def monthly_transaction_avg(data_frame):
     averageList = []
     sumList = []
-    data_frame_new = data_frame[[
-        'arrival_date_year', 'arrival_date_month', 'adr']]
-
-
-    # for index, row in data_frame_new.iterrows():
-    #     year = row['arrival_date_year']
-    #     if year not in yearlist:
-    #         yearlist.append( year )
-
     dfList = []
-    # df = "df"
-    for year in yearlist:
-        rows = []
-        for index, row in data_frame_new.iterrows():
-            y = row['arrival_date_year']
-            m = row['arrival_date_month']
-            adr = row['adr']
-            if y == int(year):
-                rows.append( [int(y), m, adr] )
-        df = pd.DataFrame( rows, columns=['year', 'month', 'adr'] )
-        averageList.append( df.groupby( ['year', 'month'], sort=False ).mean() )
-        sumList.append( df.groupby( ['year', 'month'], sort=False ).sum() )
-    #     dfList.append( df )
-
-    # print( 'Loading...................' )
-    # for dataframes in dfList:
-    #     averageList.append( dataframes.groupby( ['year', 'month'], sort=False ).mean() )
-    #     sumList.append( dataframes.groupby( ['year', 'month'], sort=False ).sum() )
+    dfList.append( readCsv( 'year1.csv' ) )
+    dfList.append( readCsv( 'year2.csv' ) )
+    dfList.append( readCsv( 'year3.csv' ) )
+    for dataframes in dfList:
+        averageList.append( dataframes.groupby( ['year', 'month'], sort=False ).mean() )
+        sumList.append( dataframes.groupby( ['year', 'month'], sort=False ).sum() )
     print( 'Loading...................' )
     return averageList, sumList
 
-output2 = monthly_transaction_avg( toCSV( 'correct_cluster.csv' ) )
+output2 = monthly_transaction_avg( readCsv( 'correct_cluster.csv' ) )
 
 
 def dictionryOutput():
